9a490670278188e1732caccdb940adb41aa6f281
MasterUNG <phrombutr@gmail.com>
2018-03-26 03:42:03 +0700

    Show ListView Success

--- app/src/main/java/janjira/jiraporn/yonlada/aroirestuarant/fragment/ChefFragment.java
+++ app/src/main/java/janjira/jiraporn/yonlada/aroirestuarant/fragment/ChefFragment.java
@@ -9,15 +9,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import janjira.jiraporn.yonlada.aroirestuarant.R;
+import janjira.jiraporn.yonlada.aroirestuarant.utility.ChefAdapter;
 import janjira.jiraporn.yonlada.aroirestuarant.utility.GetOrderWhereDate;
 import janjira.jiraporn.yonlada.aroirestuarant.utility.MyConstanct;
 
-public class ChefFragment extends Fragment{
+public class ChefFragment extends Fragment {
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -41,6 +45,24 @@ public class ChefFragment extends Fragment{
             getOrderWhereDate.execute(currentDateString, myConstanct.getUrlGetOrderWhereDate());
 
             String jsonString = getOrderWhereDate.get();
+            JSONArray jsonArray = new JSONArray(jsonString);
+
+            String[] nameFoodStrings = new String[jsonArray.length()];
+            String[] tableStrings = new String[jsonArray.length()];
+            String[] itemeStrings = new String[jsonArray.length()];
+            String[] statusStrings = new String[jsonArray.length()];
+            String[] columnStrings = myConstanct.getColumnOrder();
+
+            for (int i = 0; i < jsonArray.length(); i += 1) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                nameFoodStrings[i] = jsonObject.getString(columnStrings[3]);
+                tableStrings[i] = "User-8";
+                itemeStrings[i] = jsonObject.getString(columnStrings[4]);
+                statusStrings[i] = jsonObject.getString(columnStrings[5]);
+            }
+
+            ChefAdapter chefAdapter = new ChefAdapter(getActivity(), nameFoodStrings, tableStrings, itemeStrings, statusStrings);
+            listView.setAdapter(chefAdapter);
 
         } catch (Exception e) {
             e.printStackTrace();
--- app/src/main/java/janjira/jiraporn/yonlada/aroirestuarant/utility/ChefAdapter.java
+++ app/src/main/java/janjira/jiraporn/yonlada/aroirestuarant/utility/ChefAdapter.java
@@ -1,12 +1,68 @@
 package janjira.jiraporn.yonlada.aroirestuarant.utility;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import janjira.jiraporn.yonlada.aroirestuarant.R;
 
 /**
  * Created by masterung on 25/3/2018 AD.
  */
 
-public class ChefAdapter {
+public class ChefAdapter extends BaseAdapter {
     Context context;
+    String[] nameFoodStrings, tableStrings, itemStrings, statusStrings;
+    private String[] strings = new String[]{"กำลังปรุง", "เสร็จแล้ว"};
 
+    public ChefAdapter(Context context,
+                       String[] nameFoodStrings,
+                       String[] tableStrings,
+                       String[] itemStrings,
+                       String[] statusStrings) {
+        this.context = context;
+        this.nameFoodStrings = nameFoodStrings;
+        this.tableStrings = tableStrings;
+        this.itemStrings = itemStrings;
+        this.statusStrings = statusStrings;
+    }
+
+    @Override
+    public int getCount() {
+        return nameFoodStrings.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.listview_chef, parent, false);
+
+        TextView nameFoodTextView = view.findViewById(R.id.txtNamefood);
+        TextView tableTextView = view.findViewById(R.id.txtNameTable);
+        TextView itemTextView = view.findViewById(R.id.txtItem);
+        TextView statusTextView = view.findViewById(R.id.txtStatus);
+
+        nameFoodTextView.setText(nameFoodStrings[position]);
+        tableTextView.setText(tableStrings[position]);
+        itemTextView.setText(itemStrings[position]);
+        statusTextView.setText(strings[Integer.parseInt(statusStrings[position])]);
+
+
+
+        return view;
+    }
 }
